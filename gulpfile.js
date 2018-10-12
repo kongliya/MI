c69b2bbdbc561186470c71edad69d550bc04fc28
kongliya <kl71611@163.com>
2018-10-12 11:45:40 +0800

    第一次提交购物车结构+布局

--- gulpfile.js
+++ gulpfile.js
@@ -25,7 +25,7 @@ gulp.task("html",()=>{
 
 gulp.task('connect',function(){
 	connect.server({
-		port:88,
+		port:82,
 		root:"dist/",
 		livereload:true,
 		// 中间件;
@@ -69,7 +69,7 @@ gulp.task("sass",()=>{
 	// sass 只要出错 connect就会终止;
 	// 我们想要的只是报错 但是不退出连接;
 	.pipe(sass().on("error",sass.logError))
-	.pipe(gulp.dest("dist.css"))
+	.pipe(gulp.dest("dist/css"))
 })
 
 // 编译 ? es6 => es5;
